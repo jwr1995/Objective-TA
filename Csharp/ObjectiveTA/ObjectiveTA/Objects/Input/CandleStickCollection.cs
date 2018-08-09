1a99bff9c86aacaacb0078029aab146378cb73ee
William Ravenscroft <jwravenscroft@gmail.com>
2018-08-09 21:49:32 +0100

    Folder swaps

---
+++ Csharp/ObjectiveTA/ObjectiveTA/Objects/Input/CandleStickCollection.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace ObjectiveTA.Model.Input
+{
+    public class CandleStickCollection : Collection<CandleStick>
+    {
+        public CandleStickCollection(){}
+
+        public CandleStickCollection(List<CandleStick> candleSticks)
+        {
+            try
+            {
+                foreach (CandleStick bin in candleSticks)
+                {
+                    this.Add(bin);
+                }   
+            }
+            catch (NullReferenceException)
+            {
+                throw;
+            }
+
+        }
+
+        public CandleStickCollection(Collection<CandleStick> candleSticks)
+        {
+            try
+            {
+                foreach (CandleStick bin in candleSticks)
+                {
+                    this.Add(bin);
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw;
+            }
+        }
+
+    }
+}
