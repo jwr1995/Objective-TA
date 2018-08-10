3db3d0ee2c08753dc71f5f1cca7e9b2a51472efe
William Ravenscroft <jwravenscroft@gmail.com>
2018-08-10 13:33:58 +0100

    Added Simple Moving Average

---
+++ Csharp/ObjectiveTA/ObjectiveTA/Indicators/MovingAverage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using ObjectiveTA.Objects.Input;
+using ObjectiveTA.Objects.Output;
+
+namespace ObjectiveTA.Indicators
+{
+    public static class MovingAverage
+    {
+        /// <summary>
+        /// Simple Moving Average
+        /// </summary>
+        /// <returns>The sma.</returns>
+        /// <param name="candleSticks">Candle sticks.</param>
+        /// <param name="period">Time period (size of candlestick time inerval * period)</param>
+        public static MAModel SMA(CandleStickCollection candleSticks,int period = 14)
+        {
+            int count = candleSticks.Count;
+            double[] sma = new double[count];
+
+            for (int j = 0; j < count - period - 1; j++)
+            {
+                double sum = 0;
+
+                for (int i = j; i < j + period - 1; i++)
+                {
+                    sum = sum + candleSticks[i].Close;
+                }
+
+                sma[j + period - 1] = sum/period;
+            }
+            
+            return new MAModel(sma, MAType.SMA);
+        }
+    }
+}
---
+++ Csharp/ObjectiveTA/ObjectiveTA/Objects/Output/MAModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using ObjectiveTA.Objects.Input;
+using ObjectiveTA.Trading;
+
+namespace ObjectiveTA.Objects.Output
+{
+    public class MAModel
+    {
+        private double[] movingAverage;
+        private MAType maType;
+
+        public MAModel(double[] movingAverage, MAType maType)
+        {
+            MovingAverage = movingAverage;
+            this.maType = maType;
+        }
+
+        public double[] MovingAverage { get => movingAverage; set => movingAverage = value; }
+        public MAType MaType { get => maType; }
+
+        public bool HasBrokeSupport(CandleStickCollection candleSticks, double support, int period = 2)
+        {
+            return true;
+        }
+
+        public bool HasBrokeResistance(CandleStickCollection candleSticks, double resistance, int period = 2)
+        {
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// MA Types:
+    /// Simple Moving Average (SMA), Exponential Moving Average (EMA),
+    /// Cumulative Moving Average (CMA), Weighted Moving Average (WMA),
+    /// Running Moving Average/Smoothed Moving Average/Modified Moving Average (RMA)
+    /// </summary>
+    public enum MAType {SMA, EMA, CMA, WMA, RMA}
+}
